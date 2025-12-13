803118a845b9bf17113e6639934e29f1e810f2bd
Mohamed Abdelmeged <mabdelmeged.se@gmail.com>
2025-12-13 12:55:35 +0300

    push program.cs

---
+++ MathGame.MohamedAbdelmeged/MathGame.MohamedAbdelmeged/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using MathGame.MohamedAbdelmeged.Helpers;
+using MathGame.MohamedAbdelmeged.Services;
+
+
+ConsoleHelper.PrintWelcome();
+
+var gameIsOpen = true;
+while (gameIsOpen)
+{
+    gameIsOpen = GameService.StartGame();
+}
+ConsoleHelper.PrintGoodBye();
